7a106a86a6561dcfd95926b07b9f23e18d8b2e1a
Artyom-Y <artem.iavchunovskii@epita.fr>
2026-05-17 21:12:51 +0200

    feat: chunking function

--- utilities.py
+++ utilities.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv, find_dotenv, set_key
 from unstructured.partition.auto import partition
-import chromadb
+from unstructured.chunking.title import chunk_by_title
+from unstructured.cleaners.core import clean
 
 # .env file interaction
 def create_env_if_not_exists():
@@ -9,11 +10,11 @@ def create_env_if_not_exists():
         with open(".env", 'w') as file:
             file.write("GOOGLE_API_KEY=''")
 
-def get_google_key():
+def get_google_key() -> str:
     load_dotenv()
     return os.getenv("GOOGLE_API_KEY")
 
-def set_google_key(value):
+def set_google_key(value: str):
     path = find_dotenv()
     load_dotenv()
     set_key(path, "GOOGLE_API_KEY", value)
@@ -22,12 +23,19 @@ def set_google_key(value):
 content_types = {".pdf": "application/pdf", ".md": "text/markdown"}
 
 
-def parse_file(file):
+def parse_file(file: str) -> list[str]:
     path = os.path.join("storage\input", file)
+    if not os.path.exists(path):
+        return 0
     _, file_extension = os.path.splitext(path)
-
+    
     if file_extension in content_types.keys():
         elements = partition(filename=path, content_type=content_types[file_extension])
     else:
         elements = partition(filename=path, content_type="text/plain")
-    return elements
+    chunks = chunk_by_title(elements, max_characters=150)
+    return [clean(str(chunk), extra_whitespace=True) for chunk in chunks]
+
+# chunks = parse_file("sample.pdf")
+# for chunk in chunks:
+#     print(chunk)
