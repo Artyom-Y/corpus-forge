f2a3a6291206e106d42d96e01a1fc1cfeb7a4a2a
Artyom-Y <artem.iavchunovskii@epita.fr>
2026-05-19 19:18:46 +0200

    fix: get_chat()

--- model.py
+++ model.py
@@ -11,18 +11,26 @@ with open("config.toml", "rb") as f:
 
 model = config["model_parameters"]["name"]
 system_instruction = config["model_parameters"]["system_instruction"]
+temperature = config["model_parameters"]["temperature"]
 
 all_tools = {
-"grounding_tool": types.Tool(google_search=types.GoogleSearch()),
-"google_maps_tool": types.Tool(google_maps=types.GoogleMaps()),
-"url_context_tool": types.Tool(url_context=types.UrlContext())
+"google_search": types.Tool(google_search=types.GoogleSearch()),
+"google_maps": types.Tool(google_maps=types.GoogleMaps()),
+"url_context": types.Tool(url_context=types.UrlContext())
 }
 
 def get_chat():
-    client = genai.Client(api_key=get_google_key())
+    client = genai.Client(api_key=get_google_key()) # ValueError if key is incorrect/not provided
     history = read_history()
+
     tools = [tool for name, tool in all_tools.items() if config["tools"][name]]
-    chat = client.chats.create(model=model, history=history)
-    
-    
-    
+    model_info = client.models.get(model=model)
+    config_dict = {}
+    if len(system_instruction) > 0:
+        config_dict["system_instruction"] = system_instruction
+    if tools:
+        config_dict["tools"] = tools
+    if temperature in [0.0, model_info.max_temperature]:
+        config_dict["temperature"] = temperature
+    chat_config = types.GenerateContentConfig(**config_dict)
+    return client.chats.create(model=model, history=history, config=chat_config)
